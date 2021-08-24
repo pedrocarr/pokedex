6fd122369ebe5c4de071292f790f6adee6c0721a
Pedro Carvalho <eng.pedro.amb@gmail.com>
2021-08-24 14:55:00 -0300

    initial pokedex

--- src/app/pokemon-card/pokemon-card.component.ts
+++ src/app/pokemon-card/pokemon-card.component.ts
@@ -7,6 +7,16 @@ import { Pokemon } from 'src/_model/Pokemon';
   styleUrls: ['./pokemon-card.component.sass']
 })
 export class PokemonCardComponent {
-  @Input('pokemon')
+  @Input()
   public pokemon: Pokemon;
+
+  public leadingZero(str: string | number, size: number = 3): string {
+    let s = String(str);
+
+    while (s.length < (size || 2)) {
+      s = '0' + s;
+    }
+
+    return s;
+  }
 }
